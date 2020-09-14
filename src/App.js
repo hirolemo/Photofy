7b8fc61d3fb0188e338bd8659fa9d2620c7ba5a7
Mohamed <hirole.mohamed@gmail.com>
2020-09-14 19:36:27 -0400

    Added JS files and links for Firebase auth and sign up/in functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import UploadForm from './comps/UploadForm';
 import Title from './comps/Title';
 import ImageGrid from './comps/ImageGrid';
 import Modal from './comps/Modal';
+import Navbar from './comps/layout/Navbar';
+import Navbar from './comps/auth/SignIn';
+import Navbar from './comps/auth/SignUp';
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>Photofy</h1>
       <h2>Your Gallery</h2>
-      <p>Welcome to Photfy! Upload and share your pictures here with others!.</p>
+      <p>Welcome to Photofy! Upload and share your pictures here with others!.</p>
     </div>
   )
 }
