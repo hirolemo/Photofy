7fdcce1df977dd30c5f09f09524242177aa1b867
Mohamed <hirole.mohamed@gmail.com>
2020-09-14 14:08:17 -0400

    Added event handling so backdrop clicks exit enlarged image view, while clicking on image doesn't -> Modal.js/App.js

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,3 +1,20 @@
 import React from 'react';
 
-//modal to display large image upon clicking of an image in gallery
+//modal to display large image upon clicking of an image in gallery upon click event
+
+const Modal = ({selectedImg, setSelectedImg}) => {
+    //handle clicking of backdrop to close enlarged image after viewing
+    const handleClick = (e) => {
+        if(e.target.classList.contains('backdrop')) {
+            setSelectedImg(null);
+        }
+        
+    }
+    return (
+        <div className="backdrop" onClick={handleClick}>
+            <img src={selectedImg} alt="popup" />
+        </div>
+    )
+}
+
+export default Modal;
--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,8 +4,8 @@ const Title = () => {
   return (
     <div className="title">
       <h1>Photofy</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Your Gallery</h2>
+      <p>Welcome to Photfy! Upload and share your pictures here with others!.</p>
     </div>
   )
 }
